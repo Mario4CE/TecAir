c8af1f2c71f6977a2598934bcbe13df0e4ef1aaa
Mario <mario28.ce@gmail.com>
2026-05-13 20:51:54 -0600

    Algunos ajustes

--- tecair-cliente/js/api.js
+++ tecair-cliente/js/api.js
@@ -3,7 +3,7 @@
  */
 
 // URL base de la API (cambiar según necesidad)
-const API_BASE_URL = 'http://localhost:3000/api';
+const API_BASE_URL = 'http://localhost:5000/api';
 
 /**
  * Realiza una petición GET a la API
